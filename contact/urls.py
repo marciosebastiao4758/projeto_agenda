feeb739287f1fd749b974246a0787dc2673dacec
marciosebastia4758 <marciosebastiao302626@hotmail.com>
2024-07-18 16:25:45 -0300

    criando a busca search

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, contact, search
 
 app_name = "contact"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("contact/<int:contact_id>/", contact, name="contact"),
+    path("search/", search, name="search"),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact, Category
+from django.db.models import Q
+
 
 def index(request):
-    contacts = Contact.objects.all()
+    # filtrando os contatos pelo campo show=True e ordenando pelo id e fatiando
+    contacts = Contact.objects\
+        .filter(show=True)\
+            .order_by("-id")[:10]
+            # printando a consulta sql no terminal
+    # print(contacts.query)
+        
+    return render(request, "contact/index.html", {"contacts":contacts})
+
+def contact(request, contact_id):
+    single_contact = Contact.objects.get(pk=contact_id)
+    return render(request, "contact/contact.html", {"contact":single_contact})
+
+
+def search(request):
+    # pegando o valor do request com request.GET sendo que get("q", "") está pegando valor do nome
+    search_value = request.GET.get("q", "").strip()
+    if search_value == "":
+        return redirect("contact:index")
+        
+            # printando a consulta sql no terminal
+    # print("search_value", search_value)
+    # filtrando os contatos pelo campo show=True e ordenando pelo id e fatiando
+    contacts = Contact.objects\
+        .filter(show=True)\
+            .filter(
+                Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value) |
+                Q(email__icontains=search_value) |
+                Q(phone_icontains=search_value) 
+                )\
+            .order_by("-id")
+    # printando a consulta:
+    print(contacts.query)
+        
     return render(request, "contact/index.html", {"contacts":contacts})
