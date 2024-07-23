e4ce82621e6884b0f6f6236dcff2600218db9e9e
marciosebastia4758 <marciosebastiao302626@hotmail.com>
2024-07-23 11:44:24 -0300

    ocultando os botões de delete update

--- contact/views.py
+++ contact/views.py
@@ -71,7 +71,9 @@ def create(request):
             # contact.save()
             # print("O formulário é válido")        
             
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
              
             return redirect("contact:update", contact_id = contact.pk)
         
@@ -89,7 +91,8 @@ def create(request):
 
 @login_required(login_url="contact:login")
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk = contact_id, show=True)
+    contact = get_object_or_404(Contact, pk = contact_id, show=True,
+                                owner=request.user)
     form_action = reverse('contact:update', args=(contact_id,))
     if request.method == "POST":
         form = ContactForm(request.POST,request.FILES,instance=contact)
@@ -111,7 +114,8 @@ def update(request, contact_id):
 
 @login_required(login_url="contact:login")
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk = contact_id, show=True)
+    contact = get_object_or_404(Contact, pk = contact_id, 
+                                show=True, owner = request.user)
     confirmation = request.POST.get("confirmation", "no")
     if confirmation == "yes":
         contact.delete()
